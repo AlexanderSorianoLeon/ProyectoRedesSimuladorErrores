d14ccff80b822abcace07beb61425fe61b3e6e14
fernan0502 <142690777+fernan0502@users.noreply.github.com>
2024-08-21 00:24:20 -0500

    Update Codigo_Cliente_Servidor.py

--- Codigo_Cliente_Servidor.py
+++ Codigo_Cliente_Servidor.py
@@ -7,19 +7,19 @@ paquetes = [(i, len(paquete), paquete) for i, paquete in enumerate(paquetes)]
 
 def simular_errores(paquetes):
     tipo_error = random.random()
-    if tipo_error < 0.2:  
+    if tipo_error < 0.2:  #20% de probabilidad de que este bien todo
         return paquetes, "Mensaje recibido correctamente"
-    elif tipo_error < 0.4:  
+    elif tipo_error < 0.4:  #20% de probabilidad de perdida de paquete
         paquetes = [paquete for paquete in paquetes if random.random() < 0.8]
         return paquetes, "Error: Pérdida de paquete"
-    elif tipo_error < 0.6: 
-        paquetes = paquetes[:] 
-        random.shuffle(paquetes)  
+    elif tipo_error < 0.6: #20% de probabilidad de envio fuera de orden
+        paquetes = paquetes[:] # se crea una copia de la lista
+        random.shuffle(paquetes)  # se desordena la lista
         return paquetes, "Error: Envío fuera de orden"
     elif tipo_error < 0.8: 
         paquetes = [(secuencia, longitud, ''.join([chr(ord(c) + random.randint(-1, 1)) for c in paquete])) for secuencia, longitud, paquete in paquetes]
         return paquetes, "Error: Falta de integridad de mensaje"
-    else:  
+    else:  # 20% de probabilidad de error combinado
         paquetes = [paquete for paquete in paquetes if random.random() < 0.8]
         paquetes = paquetes[:]  
         random.shuffle(paquetes)  
